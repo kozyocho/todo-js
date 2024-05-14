a19a92b142b7434a6961c4d8c42932d9a67df4d9
kozyocho <shohei615615@gmail.com>
2024-05-14 17:45:21 +0400

    ToDo完了機能追加

--- index.js
+++ index.js
@@ -10,15 +10,45 @@ const onClickAdd = () => {
 
     //div生成
     const div = document.createElement("div");
-    div.className = "list-row;"
+    div.className = "list-row"
 
     //p生成
     const p = document.createElement("p");
     p.className = "todo-item";
     p.innerText = inputText;
 
+    //完了ボタン生成
+    const completeButton = document.createElement("button");
+    completeButton.innerText = "完了";
+    completeButton.addEventListener("click", () => {
+        //押された完了ボタンの親にあるliタグ配下の完了ボタンと削除ボタンを削除
+        const moveTarget = completeButton.closest("li");
+        completeButton.nextElementSibling.remove();
+        completeButton.remove();
+
+        //戻すボタン生成してdivタグ配下に設置
+        const backButton = document.createElement("button");
+        backButton.innerText = "戻す";
+        moveTarget.firstElementChild.appendChild(backButton);
+
+        //完了リストに移動
+        document.getElementById("complete-list").appendChild(moveTarget);
+    });
+
+    //削除ボタン生成
+    const deleteButton = document.createElement("button");
+    deleteButton.innerText = "削除";
+    //押された削除ボタンの親にあたるliを未完了リストから削除
+    deleteButton.addEventListener("click", () => {
+        const deleteTarget = deleteButton.closest("li");
+        document.getElementById("incomplete-list").removeChild(deleteTarget);
+    })
+
+
     //liタグ子要素に各要素を設定
     div.appendChild(p);
+    div.appendChild(completeButton);
+    div.appendChild(deleteButton);
     li.appendChild(div);
 
     //未完了リストに追加
